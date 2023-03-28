4ab99e22024a3afc0652d22fc488bb97d01ac518
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2023-03-27 20:31:36 -0500

    fixed map name highlighting

---
+++ script.js
@@ -0,0 +1,46 @@
+// Get all the area elements
+const areas = document.querySelectorAll("area");
+
+// get the image map
+const mapArea = document.querySelector(`[data-id="image-map"]`);
+
+// get the bounding box of the map area
+const boundingBox = mapArea.getBoundingClientRect();
+
+// Add mouseover and mouseout event listeners to each area
+areas.forEach((area) => {
+    area.addEventListener("mouseover", mouseOver);
+    area.addEventListener("mouseout", mouseOut);
+});
+
+function mouseOver(e) {
+    const target = e.target;
+    const coords = target.getAttribute("coords").split(",");
+
+    //get the position and height based off of the coords
+    const left = parseInt(coords[0]);
+    const top = parseInt(coords[1]);
+    const width = parseInt(coords[2]) - left;
+    const height = parseInt(coords[3]) - top;
+
+    const highlight = document.createElement("div");
+    highlight.classList.add("highlight");
+
+    // Set the position and size of the bounding box div
+    highlight.style.position = "absolute";
+    highlight.style.top = top + "px";
+    highlight.style.left = left + "px";
+    highlight.style.width = width + "px";
+    highlight.style.height = height + "px";
+
+    document.body.appendChild(highlight);
+}
+
+function mouseOut() {
+    // Remove the SVG when the mouse is no longer hovering over the area
+    const highlight = document.querySelector(".highlight");
+
+    if (highlight === null) return;
+
+    highlight.remove();
+}
