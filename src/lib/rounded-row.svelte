9b2855dcef51558218ebefad856425b9d0a59717
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2023-05-05 16:05:07 -0500

    rounded flex row component

---
+++ src/lib/rounded-row.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    //component props
+    export let classes: string = "";
+</script>
+
+<div class="bg-slate-700 border-[3px] border-gray-600 flex rounded-lg overflow-hidden {classes}">
+    <slot />
+</div>
