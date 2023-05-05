b4cce6274c986207ddd208b237d21dcc4fb0767d
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2023-05-05 16:05:27 -0500

    search page

---
+++ src/routes/search/+page.server.ts
@@ -0,0 +1,32 @@
+import { readFileSync } from "fs";
+const people: Array<any> = JSON.parse(readFileSync("people.json") as unknown as string);
+
+//TODO: find a different way to filter through the people array, current way may cause problems with a bigger list
+
+/** @type {import('./$types').PageLoad} */
+export function load({ url }: any) {
+    //get the params from the url
+    const params: { [key: string]: string } = {
+        name: url.searchParams.get("name") || null,
+        county: url.searchParams.get("county") || null,
+    };
+    //allows the user to know if their search worked
+    let validSearch: boolean = false;
+    //to be filtered by the params
+    let filtered: Array<any> = people;
+
+    Object.keys(params).forEach((param: string) => {
+        //check to make sure the param value is valid
+        if (typeof params[param] !== "string" || params[param].length < 1) return;
+
+        //was a valid search as it contained a valid param
+        validSearch = true;
+        //filter out the param
+        filtered = filtered.filter((person: any) => person[param].includes(params[param]));
+    });
+
+    return {
+        validSearch,
+        people: validSearch !== false ? filtered : null,
+    };
+}
---
+++ src/routes/search/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    //import the components
+    import SearchBar from "$lib/search-bar.svelte";
+    import PersonCard from "$lib/person-card.svelte";
+
+    //contains the list of people
+    export let data: { validSearch: boolean; people: Array<any> } = { validSearch: true, people: [] };
+</script>
+
+<section class="h-full">
+    <header class="flex flex-col justify-center items-center gap-8 h-[40%] px-5 py-[15%] md:py-10">
+        <h1 class="text-5xl">Search</h1>
+        <SearchBar classes="w-[80%]" />
+    </header>
+
+    <div class="h-full pt-[15%] md:pt-10 bg-zinc-900">
+        {#if data.validSearch === true}
+            <ul class="flex flex-wrap justify-center items-start gap-8">
+                {#each data.people as person}
+                    <PersonCard {person} />
+                {/each}
+            </ul>
+        {:else}
+            <div>
+                <h2>invalid seach</h2>
+                <p>your search pararmeters seem to be invalid, maybe try some different ones?</p>
+            </div>
+        {/if}
+    </div>
+</section>
