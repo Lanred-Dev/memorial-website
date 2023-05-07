bc71ff44d5beaac436ff1e4f5e749aef65ebfda7
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2023-05-06 20:38:50 -0500

    toggle button component

---
+++ src/lib/button/toggle.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    //component props
+    export let classes: string = "";
+    export let variable: boolean = false;
+
+    function toggle() {
+        variable = !variable;
+    }
+</script>
+
+<button class={classes} on:click={toggle}><slot /></button>
