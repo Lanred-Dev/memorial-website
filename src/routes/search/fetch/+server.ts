a6f82ee479effc281a9056a231c1056f6571afa3
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2023-05-07 09:12:10 -0500

    fetch endpoint

---
+++ src/routes/search/fetch/+server.ts
@@ -0,0 +1,14 @@
+import { readFileSync } from "fs";
+const people: Array<any> = JSON.parse(readFileSync("people.json") as unknown as string);
+
+/** @type {import('./$types').RequestHandler} */
+export function GET({ url }: any) {
+    const params: { [key: string]: number } = {
+        start: Number(url.searchParams.get('start') || 0),
+        end: Number(url.searchParams.get('end') || 0),
+    };
+    let people = "";
+
+    
+    return new Response();
+}
