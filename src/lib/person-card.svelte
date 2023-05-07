a8a79899c906428599e9836b3d07cfc33cedb0c6
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2023-05-06 20:40:18 -0500

    caniuse-lite & reworked card component

--- src/lib/person-card.svelte
+++ src/lib/person-card.svelte
@@ -1,18 +1,55 @@
 <script lang="ts">
+    //component props
     export let person: any;
+    export let compact: boolean = false;
 
+    import { onMount } from "svelte";
     import { format } from "date-fns";
+
+    let startingCompact: boolean = compact;
+
+    onMount(() => {
+        startingCompact = compact
+
+        if (startingCompact === true) return;
+
+        //update the height whenever the window is resized
+        window.addEventListener("resize", () => {
+            if (window.innerWidth <= 500) {
+                compact = true;
+            } else {
+                compact = false;
+            }
+        });
+    });
 </script>
 
-<li class="w-32 aspect-square relative group">
-    <img class="w-full h-full rounded-md group-hover:rounded-r-none" src={person.picture} alt={person.name} />
+{#if compact === false}
+    <li class="w-[100%] lg:w-[40%] h-36 rounded-lg bg-slate-700 border-[3px] border-gray-600 flex overflow-hidden">
+        <img class="h-full group-hover:rounded-r-none" src={person.picture} alt={person.name} />
 
-    <section class="w-auto h-full bg-black absolute right-0 translate-x-full top-0 px-8 py-2 hidden flex-col justify-between items-start group-hover:flex z-[1] text-left whitespace-nowrap">
-        <header>
-            <p class="text-lg">{person.name}</p>
-            <p class="text-sm">{format(person.born, "MM/dd/yyyy")} - {format(person.died, "MM/dd/yyyy")}</p>
-        </header>
+        <!--info container-->
+        <section class="w-full h-full px-10 py-5 text-left">
+            <header class="mb-2">
+                <p class="text-lg">{person.name}</p>
+                <p class="text-sm text-gray-300">{format(person.dob, "MM/dd/yyyy")} - {format(person.dod, "MM/dd/yyyy")}</p>
+            </header>
 
-        <button class="px-4 py-2 rounded-md bg-[#bf0a30] text-base">see profile</button>
-    </section>
-</li>
+            <p>{person.name}</p>
+        </section>
+    </li>
+{:else}
+    <li class="w-28 lg:w-36 aspect-square rounded-lg overflow-hidden">
+        <img class="h-full group-hover:rounded-r-none" src={person.picture} alt={person.name} />
+
+        <!--info container-->
+        <section class="w-full h-full px-10 py-5 text-left hidden">
+            <header class="mb-2">
+                <p class="text-lg">{person.name}</p>
+                <p class="text-sm text-gray-300">{format(person.dob, "MM/dd/yyyy")} - {format(person.dod, "MM/dd/yyyy")}</p>
+            </header>
+
+            <p>{person.name}</p>
+        </section>
+    </li>
+{/if}
