85437560447275abae9afde9155d652545f6ec45
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2023-05-06 20:37:56 -0500

    people list component

---
+++ src/lib/person-list.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    //import components
+    import PersonCard from "./person-card.svelte";
+
+    //component props
+    export let people: any;
+    export let compact: boolean = false;
+</script>
+
+<ul class="flex flex-wrap justify-center items-start gap-10 px-10 lg:px-0">
+    {#each people as person}
+        <PersonCard {person} {compact} />
+    {/each}
+</ul>
