060f4f0722e662fbd57d4cf418496c2bdf4b2732
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2023-05-08 16:07:58 -0500

    clear button for inputs

--- src/lib/form/input.svelte
+++ src/lib/form/input.svelte
@@ -5,6 +5,42 @@
     export let id: string = "";
     export let type: string = "string";
     export let value: any = "";
+
+    import { onMount } from "svelte";
+    let input: HTMLInputElement;
+    let clearButton: HTMLButtonElement;
+
+    function clear() {
+        input.value = "";
+        toggleClearButton(false);
+    }
+
+    function toggleClearButton(enabled: boolean) {
+        if (enabled === true) {
+            clearButton.style.display = "block";
+        } else if (enabled === false) {
+            clearButton.style.display = "none";
+        }
+    }
+
+    function focus() {
+        console.log("A")
+        if (input.value.length === 0) return toggleClearButton(false);
+
+        toggleClearButton(true);
+    }
+
+    function unfocus() {
+        toggleClearButton(false);
+    }
+
+    onMount(unfocus);
 </script>
 
-<input class="outline-none placeholder-[#F5F5F5] {classes}" placeholder={placeholder} type={type} value={value} data-id={id} />
+<div class="relative {classes}">
+    <input class="outline-none placeholder-[#F5F5F5] w-full bg-transparent" placeholder={placeholder} type={type} value={value} data-id={id} bind:this={input} on:focusin={focus} on:focusout={unfocus} on:keypress={focus} />
+
+    <button class="h-[60%] aspect-square absolute right-2 top-1/2 -translate-y-1/2" on:click={clear} bind:this={clearButton}>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="fill-[#F5F5F5]"><path d="M12.0007 10.5865L16.9504 5.63672L18.3646 7.05093L13.4149 12.0007L18.3646 16.9504L16.9504 18.3646L12.0007 13.4149L7.05093 18.3646L5.63672 16.9504L10.5865 12.0007L5.63672 7.05093L7.05093 5.63672L12.0007 10.5865Z"></path></svg>
+    </button>
+</div>
