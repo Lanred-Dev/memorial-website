28c9c7a45ec0cc37df9039f82c6fdeb7c8bb3f4f
Landon Redmond <76853738+Lanred-Dev@users.noreply.github.com>
2023-05-08 07:58:15 -0500

    fix comments

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Get all the area elements
+// get all the area elements
 const areas = document.querySelectorAll("area");
 
 // get the image map
@@ -7,7 +7,7 @@ const mapArea = document.querySelector(`[data-id="image-map"]`);
 // get the bounding box of the map area
 const boundingBox = mapArea.getBoundingClientRect();
 
-// Add mouseover and mouseout event listeners to each area
+// add mouseover and mouseout event listeners to each area
 areas.forEach((area) => {
     area.addEventListener("mouseover", mouseOver);
     area.addEventListener("mouseout", mouseOut);
@@ -26,7 +26,7 @@ function mouseOver(e) {
     const highlight = document.createElement("div");
     highlight.classList.add("highlight");
 
-    // Set the position and size of the bounding box div
+    // set the position and size of the bounding box div
     highlight.style.position = "absolute";
     highlight.style.top = top + "px";
     highlight.style.left = left + "px";
@@ -37,7 +37,7 @@ function mouseOver(e) {
 }
 
 function mouseOut() {
-    // Remove the SVG when the mouse is no longer hovering over the area
+    // remove the SVG when the mouse is no longer hovering over the area
     const highlight = document.querySelector(".highlight");
 
     if (highlight === null) return;
