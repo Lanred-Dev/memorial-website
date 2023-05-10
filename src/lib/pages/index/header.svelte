0c91ef560cb26456880d7654447bdaacb651a23b
Landon Redmond <76853738+Lanred-Dev@users.noreply.github.com>
2023-05-10 09:08:03 -0500

    fix header heights on smaller screens

--- src/lib/pages/index/header.svelte
+++ src/lib/pages/index/header.svelte
@@ -15,7 +15,7 @@
     });
 </script>
 
-<header class="bg-backgroundSecondary h-full lg:h-[80vh] flex flex-col justify-center items-center gap-8 px-12 py-[15%] md:py-10 relative">
+<header class="bg-backgroundSecondary h-screen lg:h-[80vh] flex flex-col justify-center items-center gap-8 px-12 py-[15%] md:py-10 relative">
     <div class="flex flex-col items-center gap-4 z-[1]">
         <h1 class="text-4xl md:text-6xl">Cullman Veteran Memorial</h1>
         <p class="text-lg md:text-2xl lg:max-w-[60%]">The Cullman Veteran Memorial is dedicated to honoring every person whose name is insribed on the Cullman Veteran Memorial in Cullman, AL.</p>
