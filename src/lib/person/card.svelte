220b72418c95b66c943423809ba36cb47681bcd0
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2023-05-11 22:23:49 -0500

    revamp card component; closes #14 & closes #25

--- src/lib/person/card.svelte
+++ src/lib/person/card.svelte
@@ -3,53 +3,44 @@
     export let person: any;
     export let compact: boolean = false;
 
-    import { onMount } from "svelte";
+    import { onDestroy, onMount } from "svelte";
     import { format } from "date-fns";
 
-    let startingCompact: boolean = compact;
+    //determine if we need to swap to compact mode due to screen size
+    function determineCompact() {
+        if (window.innerWidth <= 500) {
+            compact = true;
+        } else {
+            compact = false;
+        }
+    }
 
     onMount(() => {
-        startingCompact = compact
+        //already compact no need
+        if (compact === true) return;
 
-        if (startingCompact === true) return;
+        window.addEventListener("resize", determineCompact);
+    });
 
-        //update the height whenever the window is resized
-        window.addEventListener("resize", () => {
-            if (window.innerWidth <= 500) {
-                compact = true;
-            } else {
-                compact = false;
-            }
-        });
+    onDestroy(() => {
+        //we never created an event as it is already compact
+        if (compact === true) return;
+
+        window.removeEventListener("resize", determineCompact);
     });
 </script>
 
-{#if compact === false}
-    <li class="w-[100%] lg:w-[40%] h-36 rounded-lg bg-slate-700 border-[3px] border-gray-600 flex overflow-hidden">
-        <img class="h-full group-hover:rounded-r-none" src={person.picture} alt={person.name} />
+<li class="border-[3px] rounded-lg border-modalSecondary flex overflow-hidden {compact === false ? 'w-[100%] lg:w-[40%] h-36 bg-backgroundSecondaryComplement' : 'w-28 lg:w-36 aspect-square'}">
+    <img class="h-full group-hover:rounded-r-none" src={person.picture} alt={person.name} />
 
-        <!--info container-->
-        <section class="w-full h-full px-10 py-5 text-left">
+    {#if compact === false}
+        <section class="w-full h-full px-7 p-5 text-left border-l-[2px] border-modalSecondary">
             <header class="mb-2">
                 <p class="text-lg">{person.name}</p>
-                <p class="text-sm text-gray-300">{format(person.dob, "MM/dd/yyyy")} - {format(person.dod, "MM/dd/yyyy")}</p>
+                <p class="text-sm text-secondary">{format(person.dob, "MM/dd/yyyy")} - {format(person.dod, "MM/dd/yyyy")}</p>
             </header>
 
-            <p>{person.name}</p>
+            <p>{person.bio !== undefined ? person.bio : "This person has no bio."}</p>
         </section>
-    </li>
-{:else}
-    <li class="w-28 lg:w-36 aspect-square rounded-lg overflow-hidden">
-        <img class="h-full group-hover:rounded-r-none" src={person.picture} alt={person.name} />
-
-        <!--info container-->
-        <section class="w-full h-full px-10 py-5 text-left hidden">
-            <header class="mb-2">
-                <p class="text-lg">{person.name}</p>
-                <p class="text-sm text-gray-300">{format(person.dob, "MM/dd/yyyy")} - {format(person.dod, "MM/dd/yyyy")}</p>
-            </header>
-
-            <p>{person.name}</p>
-        </section>
-    </li>
-{/if}
+    {/if}
+</li>
