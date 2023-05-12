65f691865bd98e274959ab5a0af29398d0dd1cbe
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2023-05-12 16:43:03 -0500

    fixes #33

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,11 +5,51 @@
     import { onMount } from "svelte";
 
     let heightScreenElements: HTMLElement[];
+    let classRegex: RegExp = /(lg|sm|md)\:h\-/gm;
+    let selectorRegex: RegExp = /(lg|sm|md)/gm;
+
+    function checkIfSelectorMatch(element: HTMLElement): boolean {
+        const classMatches: any = classRegex.exec(element.classList.toString());
+
+        if (classMatches === null) return false;
+
+        let result: boolean = false;
+
+        classMatches.forEach((string: string) => {
+            const selectorMatches: any = selectorRegex.exec(string);
+
+            if (selectorMatches === null) {
+                result = result === true ? true : false;
+                return;
+            }
+
+            switch (selectorMatches[0]) {
+                case "lg":
+                    result = window.innerWidth > 1024;
+                    break;
+                case "md":
+                    result = window.innerWidth > 768;
+                    break;
+                case "sm":
+                    result = window.innerWidth > 640;
+                    break;
+            }
+        });
+
+        return result;
+    }
 
     //fixes bug on IOS where the viewport space is bigger than the innerHeight
     function updateHeightElements() {
         heightScreenElements = Array.from(document.querySelectorAll(".h-screen") as unknown as HTMLCollectionOf<HTMLElement>);
         heightScreenElements.forEach((element: HTMLElement) => {
+            const selectorMatch: boolean = checkIfSelectorMatch(element);
+
+            if (selectorMatch === true) {
+                element.style.height = null as any;
+                return;
+            };
+
             element.style.height = `${window.innerHeight}px`;
         });
     }
