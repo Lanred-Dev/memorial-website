b337e526aa549a03bbda0f902641141a409db396
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2023-06-03 14:56:51 -0500

    add support for links & bigger highlights

--- src/lib/components/label/highlight.svelte
+++ src/lib/components/label/highlight.svelte
@@ -1,3 +1,17 @@
-<span class="rounded-sm bg-highlight px-0.5 font-bold text-primary">
-    <slot />
-</span>
+<script lang="ts">
+    //component props
+    export let link: string | null = null;
+    export let biggerHighlight: boolean = false;
+
+    let classes = biggerHighlight == true ? "rounded-md bg-highlight px-2 font-bold text-primary" : "rounded-sm bg-highlight px-0.5 font-bold text-primary";
+</script>
+
+{#if link !== null}
+    <a class={classes} href={link}>
+        <slot />
+    </a>
+{:else}
+    <span class={classes}>
+        <slot />
+    </span>
+{/if}
