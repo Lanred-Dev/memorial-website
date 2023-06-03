005095782c5772dcb550d9a44efaea79a3f765cc
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2023-06-03 14:56:31 -0500

    change header title

--- src/lib/pages/search/header.svelte
+++ src/lib/pages/search/header.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
     //import components
     import SearchBar from "$lib/components/search-bar.svelte";
+    import Highlight from "$lib/components/label/highlight.svelte";
 
     //component props
     export let isSearch: boolean = false;
 </script>
 
-<header class="flex {isSearch == false ? "h-screen" : "h-[70vh]"} flex-col items-center justify-center gap-8 bg-backgroundSecondary px-5 py-[15%] md:py-10">
-    <h1 class="text-4xl md:text-6xl">Search</h1>
+<header class="flex {isSearch == false ? 'h-screen' : 'h-[70vh]'} flex-col items-center justify-center gap-8 bg-backgroundSecondary px-5 py-[15%] md:py-10">
+    <h1 class="text-4xl md:text-6xl">Search for a <Highlight biggerHighlight={true}>Veteran</Highlight></h1>
     <SearchBar classes="w-[80%]" simpleSearch={false} />
 </header>
